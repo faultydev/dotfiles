ab603710535de3983131bef67cb287b575ba66b0
faulty <dev@faulty.nl>
2022-02-01 15:00:37 +0100

    script

--- sync.sh
+++ sync.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
+function clean {
+	rm /tmp/awesome-copycats
+}
+
+function awesomeUpdate {
+	echo "> awesomeUpdate"
+	git clone \
+                --recurse-submodules --remote-submodules --depth 1 -j 2 \
+                https://github.com/lcpz/awesome-copycats.git /tmp/awesome-copycats \
+		&> /dev/null
+        mkdir -p ~/.config/awesome
+        mv /tmp/awesome-copycats ~/.config/awesome
+        cp ./configfiles/awesome-rc.lua ~/.config/awesome/rc.lua
+        rm -rf /tmp/awesome-copycats
+}
+
+function symlinks {
+	echo "> symlinks"
+	ln -s configfiles/zshrc ~/.zshrc
+}
+
 git fetch
-ln -s .config/awesome 	~/.config/awesome
-ln -s .zshrc 		~/.zshrc
+
+clean
+if [[ $1 != "--no-awesome-update" ]]; then
+	awesomeUpdate
+fi
+symlinks
+
 echo "done!"
